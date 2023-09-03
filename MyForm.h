0cb422155facc9388d4d5e2176168dd0741e3e3c
Iliya Lim <limilia248@gmail.com>
2023-09-03 11:57:55 +0600

    commit1

--- MyForm.h
+++ MyForm.h
@@ -34,6 +34,13 @@ namespace WinCalculator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +55,99 @@ namespace WinCalculator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::ControlLight;
+			this->button1->Location = System::Drawing::Point(27, 160);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(90, 50);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"1";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(118, 94);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(90, 50);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(186, 26);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(31, 16);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Iliya";
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(149, 227);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(68, 20);
+			this->checkBox1->TabIndex = 3;
+			this->checkBox1->Text = L"TODO";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			this->checkBox1->CheckedChanged += gcnew System::EventHandler(this, &MyForm::checkBox1_CheckedChanged);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(149, 325);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 4;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::AppWorkspace;
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->ClientSize = System::Drawing::Size(400, 550);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->DoubleBuffered = true;
+			this->Name = L"MyForm";
+			this->Text = L"Iliya Calculator";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private:
+		System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+			this->label1->Text = "Iliya Calculator";
+		}
+	private:
+		System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	
+	private: System::Void checkBox1_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+		if (this->checkBox1->Checked)
+			this->button1->Width = 200;
+		else this->button1->Width = 90;
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		if (this->textBox1->Text == "blue")
+			this->button1->BackColor = Color::Blue;
+		else if (this->textBox1->Text == "red")
+			this->button1->BackColor = Color::Red;
+	}
+};
 }
